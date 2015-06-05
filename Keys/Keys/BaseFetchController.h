2cde66821ea817a7cd718bed2035993a15484394
Roman Klauke <romaaan.git@gmail.com>
2015-06-05 14:57:52 +0200

    base classes for fetching

---
+++ Keys/Keys/BaseFetchController.h
@@ -0,0 +1,17 @@
+    //
+    //  BaseFetchController.h
+    //  Cocktails
+    //
+    //  Created by Roman Klauke on 02.05.15.
+    //  Copyright (c) 2015 Roman Klauke. All rights reserved.
+    //
+
+#import <UIKit/UIKit.h>
+
+@import CoreData;
+
+@interface BaseFetchController : UITableViewController <NSFetchedResultsControllerDelegate>
+
+@property(strong, nonatomic) NSFetchedResultsController *fetchedResultsController;
+
+@end
